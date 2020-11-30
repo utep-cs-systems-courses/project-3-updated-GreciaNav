a55ed14a704e923586c8f2e9f667fc717236c0a2
GreciaNav <grecianav39@gmail.com>
2020-11-29 20:43:55 -0700

    Fixed comment in main.c

--- project/main.c
+++ project/main.c
@@ -14,31 +14,31 @@ void wdt_c_handler()
   static char interrupt_count = 0;
   
   if (++interrupt_count == 62 && button_state == 0) { //Called per 1/4th of a sec, dancing sonic
+    changeMade = 1;
     turn_red_off();
     buzz_song_advance();
     interrupt_count = 0;
-    changeMade = 1;
   }
   else if (interrupt_count == 250 && button_state == 1) { //Displays Zelda scene
+    changeMade = 1;
     turn_red_off();
     buzzer_set_period(0);
     clearScreen(COLOR_BLACK);
     drawZeldaScene();
     interrupt_count = 0; 
-    changeMade = 1;
   }    
   else if (interrupt_count == 250 && button_state == 2) { //Draws diamond, changes color per sec
+    changeMade = 1;
     turn_red_off();
     buzzer_set_period(0);
     draw_diamonds();
     interrupt_count = 0;
-    changeMade = 1;
   }
   else if ((interrupt_count == 3) && button_state == 3) { //Different red intensities
+    changeMade = 1;
     buzzer_set_period(0);
     dim();
     interrupt_count = 0;
-    changeMade = 1;
   }
 }
   
