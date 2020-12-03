74fca851aa8e2e0857909c770e6c60cb6f0bcf68
GreciaNav <grecianav39@gmail.com>
2020-12-03 11:48:24 -0700

    Added a case in draw_diamonds.s

--- project/draw_diamonds.s
+++ project/draw_diamonds.s
@@ -13,9 +13,9 @@ jt:
 	.global draw_diamonds
 draw_diamonds:
 	;; range check on selector (diamond_color_state)
-	mov.b #1, r13		;placeholder to compare 1
+	mov.b #2, r13		;placeholder to compare 2
 	cmp.b &diamond_color_state, r13
-	jl case2		;if N!=V, draw_diamonds > 1
+	jl case3		;if N!=V, draw_diamonds > 2
 
 	mov.b &diamond_color_state, r13
 	add.b r13, r13
@@ -24,18 +24,21 @@ draw_diamonds:
 case0:
 	mov.b #0x0000, r12 	;COLOR_BLACK
 	call #clearScreen	;clearScreen(COLOR_BLACK)
+	add.b #1, &diamond_color_state ;diamond_color_state++
+	jmp end
+case1:	
 	mov.b #60, r12
 	mov #0xffe0, r13	;COLOR_CYAN
 	call #drawDiamond	;drawDiamond(60,COLOR_CYAN)
 	add.b #1, &diamond_color_state ;diamond_color_state++
 	jmp end
-case1:
+case2:
 	mov.b #60, r12
 	mov #0xf800, r13	;COLOR_BLUE
 	call #drawDiamond	;drawDiamond(60,COLOR_BLUE)
 	add.b #1, &diamond_color_state ;diamond_color_state++
 	jmp end
-case2:
+case3:
 	mov.b #60, r12
 	mov #0xf114, r13	;COLOR_PURPLE
 	call #drawDiamond	;drawDiamond(60,COLOR_PURPLE)
